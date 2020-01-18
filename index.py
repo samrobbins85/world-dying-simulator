9167d7a6fe0a9bb3714d22cc63893959ca1283bf
Sam Robbins <samrobbinsgb@gmail.com>
2020-01-18 13:12:21 +0000

    Try URL parameters

--- index.py
+++ index.py
@@ -1,8 +1,8 @@
-from flask import Flask, Response
+from flask import Flask, Response, request
 app = Flask(__name__)
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
-    return Response("<h1>Flask on Now</h1><p>You visited: /%s</p>" % (path), mimetype="text/html")
+    return Response("<h1>Flask on Now</h1><p>You visited: /%s" +request.args.get("username")+"</p>" % (path), mimetype="text/html")
  
