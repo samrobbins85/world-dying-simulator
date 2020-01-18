40eb3bdf372a9b469b27233219acfb9bc181e44c
Sam Robbins <samrobbinsgb@gmail.com>
2020-01-18 13:04:27 +0000

    Adding initial files

---
+++ index.py
@@ -0,0 +1,8 @@
+from flask import Flask, Response
+app = Flask(__name__)
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    return Response("<h1>Flask on Now</h1><p>You visited: /%s</p>" % (path), mimetype="text/html")
+ 
