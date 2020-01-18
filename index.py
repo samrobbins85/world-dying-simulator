7c6ca4ee1da1ee2dcd1527e07f89c45f73c9e8bb
Sam Robbins <samrobbinsgb@gmail.com>
2020-01-18 15:10:56 +0000

    Update with template

--- index.py
+++ index.py
@@ -1,11 +1,11 @@
-from flask import Flask, Response, request
+from flask import Flask, Response, request, render_template
 import sys
 app = Flask(__name__)
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
-    username = request.args.get('username')
-    print(username, file=sys.stderr)
-    return Response("<h1>Flask on Now</h1><p>You visited: /%s</p>" % (username), mimetype="text/html")
+    sea_level = request.args.get('sea_level')
+#     print(username, file=sys.stderr)
+    return render_template('test.html', sea_level=sea_level)
  
