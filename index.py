76942131831e9a1fdd028d362f4cc37bfc4be7bf
Sam Robbins <samrobbinsgb@gmail.com>
2020-01-18 13:20:10 +0000

    Test console logging

--- index.py
+++ index.py
@@ -1,8 +1,11 @@
 from flask import Flask, Response, request
+import sys
 app = Flask(__name__)
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
-    return Response("<h1>Flask on Now</h1><p>You visited: /%s" +request.args.get("username")+"</p>" % (path), mimetype="text/html")
+    username = request.args.get('username')
+    print('Hello world!', file=sys.stderr)
+    return Response("<h1>Flask on Now</h1><p>You visited: /%s</p>" % (path), mimetype="text/html")
  
