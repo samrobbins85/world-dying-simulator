68d2e9a568a505ff1cb9ce4963fff5cc94965233
Sam Robbins <samrobbinsgb@gmail.com>
2020-01-18 13:23:27 +0000

    URL parameters again

--- index.py
+++ index.py
@@ -6,7 +6,6 @@ app = Flask(__name__)
 @app.route('/<path:path>')
 def catch_all(path):
     username = request.args.get('username')
-    print('Hello world!', file=sys.stderr)
-    app.logger.info('testing info log')
+    print(username, file=sys.stderr)
     return Response("<h1>Flask on Now</h1><p>You visited: /%s</p>" % (path), mimetype="text/html")
  
