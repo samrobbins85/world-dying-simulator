91b61ea3fef41f8881acbf7eec0b247359bf9078
Sam Robbins <samrobbinsgb@gmail.com>
2020-01-18 18:16:28 +0000

    Pass parameters through

--- index.py
+++ index.py
@@ -6,9 +6,14 @@ app = Flask(__name__)
 @app.route('/<path:path>')
 def catch_all(path):
     sea_level = request.args.get('sea_level')
+    temperature = request.args.get('temperature')
     if sea_level == None:
         sea_level = 300
+    if temperature=="hot":
+        temperature=="https://world-dying.now.sh/person/hot.png"
+    else:
+        temperature=="https://world-dying.now.sh/person/cold.png"
 
 #     print(username, file=sys.stderr)
-    return render_template('test.html', sea_level=sea_level)
+    return render_template('test.html', sea_level=sea_level, temperature=temperature)
  
