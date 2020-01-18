aad2cf5567a6517394aa3755f7836cae1fd9023a
Sam Robbins <samrobbinsgb@gmail.com>
2020-01-18 15:16:45 +0000

    Try except for default

--- index.py
+++ index.py
@@ -5,8 +5,11 @@ app = Flask(__name__)
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
-    sea_level = 300
-    sea_level = request.args.get('sea_level')
+    try:
+        sea_level = request.args.get('sea_level')
+    except:
+        sea_level = 300
+
 #     print(username, file=sys.stderr)
     return render_template('test.html', sea_level=sea_level)
  
